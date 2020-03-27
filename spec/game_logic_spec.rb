e062b4f4b63f677de43da0e448cc7d0d45739cd6
ThankGod Richard <madueketf@gmail.com>
2020-03-27 15:57:48 +0100

    Fixed negative test to same positive it block

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -8,15 +8,12 @@ RSpec.describe GameLogic do
       expect(game_logic.x_player).to be_a(Player)
       expect(game_logic.o_player).to be_a(Player)
       expect(game_logic.x_player).not_to be_a(Integer)
-      expect(game_logic.o_player).not_to be_a(String)
+      expect(game_logic.o_player).not_to be_a(Integer)
     end
   end
 
-<<<<<<< HEAD
 
-=======
   describe 'Array is correct' do
->>>>>>> 1821d0fa5449c02fc07e373b9814f5170e88bf0f
     it 'Array created' do
       expect(game_logic.arr).to be_a(Array)
       expect(game_logic.arr.size).to eql(3)
@@ -36,18 +33,10 @@ RSpec.describe GameLogic do
     end
   end
 
-<<<<<<< HEAD
- 
 
-=======
   describe 'Game on initialize' do
->>>>>>> 1821d0fa5449c02fc07e373b9814f5170e88bf0f
     it 'Game on should be true until otherwise' do
       expect(game_logic.game_on).to eql(true)
-      expect(game_logic.game_on).not_to eql(false)
-    end
-
-    it 'Game on should be true until otherwise (Negative)' do
       expect(game_logic.game_on).not_to eql(false)
     end
   end
@@ -55,19 +44,13 @@ RSpec.describe GameLogic do
   describe 'Game move initialize' do
     it 'Game_move should be a number' do
       expect(game_logic.game_moves).to be_a(Integer)
-    end
-
-    it 'Game_move should be a number (Negative)' do
-      expect(game_logic.game_moves).not_to be_a(Array)
+      expect(game_logic.game_moves).not_to be_a(String)
     end
   end
 
   describe 'Print board behaves as expected' do
     it 'Print Board should return a String' do
       expect(game_logic.print_board).to be_a(String)
-    end
-
-    it 'Print Board should return a String (Negative)' do
       expect(game_logic.print_board).not_to be_a(Array)
     end
   end
@@ -86,9 +69,6 @@ RSpec.describe GameLogic do
   describe 'Verify input behaves as expected' do
     it 'Verify input should return a number' do
       expect(game_logic.verify_inputs(3)).to be_a(Integer)
-    end
-
-    it 'Verify input should return 0 if number is negative' do
       expect(game_logic.verify_inputs(-1)).to eql(0)
     end
 
@@ -101,9 +81,6 @@ RSpec.describe GameLogic do
     it 'Alternate_player Shold return the next player' do
       expect(game_logic.alternate_player(game_logic.x_player)).to eql(game_logic.o_player)
       expect(game_logic.alternate_player(game_logic.o_player)).to eql(game_logic.x_player)
-    end
-
-    it 'Alternate_player Shold return the next player (Nagative)' do
       expect(game_logic.alternate_player(game_logic.x_player)).not_to eql(game_logic.x_player)
       expect(game_logic.alternate_player(game_logic.o_player)).not_to eql(game_logic.o_player)
     end
@@ -113,10 +90,7 @@ RSpec.describe GameLogic do
     it 'Should change the value of the second element of the first array ' do
       game_logic.get_cell(2, game_logic.x_player)
       expect(game_logic.arr[0][1]).to eql(game_logic.x_player.mark)
-    end
-
-    it 'Should change the value of the second element of the first array (Negative) ' do
-      game_logic.get_cell(2, game_logic.x_player)
+      game_logic.get_cell(3, game_logic.x_player)
       expect(game_logic.arr[0][1]).not_to eql(game_logic.o_player.mark)
     end
   end
@@ -132,13 +106,9 @@ RSpec.describe GameLogic do
       game_logic.arr[0] = %w[X X 0]
       game_logic.test_draw
       expect(game_logic.game_moves).to be_within(1).of(8)
-    end
-
-    it 'Game moves should be greater than 0 (Negative)' do
-      game_logic.arr[0] = %w[X X 0]
-      game_logic.test_draw
       expect(game_logic.game_moves).not_to eql(0)
     end
+
 
     it 'Game moves should 0 when there is draw game' do
       game_logic.arr[0] = %w[X X O]
@@ -146,13 +116,6 @@ RSpec.describe GameLogic do
       game_logic.arr[2] = %w[X O X]
       game_logic.test_draw
       expect(game_logic.game_moves).to eql(0)
-    end
-
-    it 'Game moves should 0 when there is draw game (Negative)' do
-      game_logic.arr[0] = %w[X X O]
-      game_logic.arr[1] = %w[O O X]
-      game_logic.arr[2] = %w[X O X]
-      game_logic.test_draw
       expect(game_logic.game_moves).not_to be_within(1).of(8)
     end
   end
@@ -162,11 +125,6 @@ RSpec.describe GameLogic do
       game_logic.arr[0] = %w[X X O]
       game_logic.new_game
       expect(game_logic.arr[0][2]).to eql(3)
-    end
-
-    it 'Arr should be reset on new game (Negative)' do
-      game_logic.arr[0] = %w[X X O]
-      game_logic.new_game
       expect(game_logic.arr[0][2]).not_to eql('X')
     end
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,17 +8,11 @@ RSpec.describe Player do
 
     it 'Assign Player Name' do
       expect(player.name).to eql(temp_name)
-    end
-
-    it 'Assign Player Name (Negative)' do
       expect(player.name).not_to eql('David')
     end
 
     it 'Assign Player mark' do
       expect(player.mark).to eql(temp_mark)
-    end
-
-    it 'Assign Player mark (Negative)' do
       expect(player.mark).not_to eql('O')
     end
   end
